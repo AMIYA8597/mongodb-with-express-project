7a57f86b64925e6cfe0d98050f7195f223851c1d
AMIYA8597 <chowdhuryamiya41@gmail.com>
2023-12-10 19:34:15 +0530

    until connect-flash

--- classroom/server.js
+++ classroom/server.js
@@ -1,35 +1,122 @@
 const express = require("express");
 const app = express();
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
 
-app.use(cookieParser());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(cookieParser("secretcode"));
+const sessionOptions = {
+  secret: "keyboard cat",
+  resave: false,
+  saveUninitialized: true,
+  // cookie: { secure: true }
+};
+app.use(session(sessionOptions));
+app.use(flash());
 
-app.get("/getsignedcookies", (req, res) => {
-  res.cookie("color", "blue", { signed: true });
-  res.cookie("name", "Amiya", { signed: true });
-  res.cookie("age", "23", { signed: true });
-  res.send("signed cookies set");
+app.get("/register", (req, res) => {
+  let { name = "Anonymous" } = req.query;
+  req.session.name = name;
+  // console.log(req.session.name);
+  // res.send(name);
+  req.flash("success", "user register successfully");
+  res.redirect("/hello");
 });
 
-app.get("/verify", (req, res) => {
-  console.log(req.cookies);
-  console.log(req.signedCookies);
-  res.send("verified");
+app.get("/hello", (req, res) => {
+  // let {name = "Anonymous"} = req.query;
+  // res.send(`Hello ${req.session.name}`);
+  console.log(req.flash("success"));
+  res.render("page.ejs", { name: req.session.name, msg: req.flash("success") });
 });
 
-app.get("/getcookies", (req, res) => {
-  res.cookie("greet", "How r U");
-  res.cookie("talk", "r u ok?");
-  res.send("send you some cookies");
-});
+// app.get("/reqcount" , (req,res) => {
+//   if (req.session.count) {
+//     req.session.count++;
+//   } else {
+//     req.session.count =1;
+//   }
+//   res.send(`you send a reqcount for ${req.session.count} times`)
+// })
 
-app.get("/", (req, res) => {
-  console.dir(req.cookies);
-  res.send("Hi this is classroom root");
-});
+// app.get("/test", (req, res) => {
+//   res.send("test successful");
+// });
 
 app.listen(3005, () => {
   console.log("server is listening in 3005");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.use(cookieParser());
+// app.use(cookieParser("secretcode"));
+
+// app.get("/getsignedcookies", (req, res) => {
+//   res.cookie("color", "blue", { signed: true });
+//   res.cookie("name", "Amiya", { signed: true });
+//   res.cookie("age", "23", { signed: true });
+//   res.send("signed cookies set");
+// });
+
+// app.get("/verify", (req, res) => {
+//   console.log(req.cookies);
+//   console.log(req.signedCookies);
+//   res.send("verified");
+// });
+
+// app.get("/getcookies", (req, res) => {
+//   res.cookie("greet", "How r U");
+//   res.cookie("talk", "r u ok?");
+//   res.send("send you some cookies");
+// });
+
+// app.get("/", (req, res) => {
+//   console.dir(req.cookies);
+//   res.send("Hi this is classroom root");
+// });
